4f3469b4ab5bdcf9be49ade3237477a29d8e05df
MattBeepMac <dtcdvd@gmail.com>
2016-03-03 00:38:55 +0100

    asf

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
 	var topScroll = $(this).scrollTop();
-	if (topScroll >=  $(window).height() - 2*$('#ribbon').height()){
+	if (topScroll >=  $(window).height() - 1.5*$('#ribbon').height()){
 		$('#ribbon').css({
 			'transform' : 'translate(0px, '+ 0 +'px)'
 		});
