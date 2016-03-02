243e7dd174cf5a53faac0e7c9cf34d2d3268d5a4
MattBeepMac <dtcdvd@gmail.com>
2016-03-02 23:35:56 +0100

    asf

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(window).scroll(function(){
+	var topScroll = $(this).scrollTop();
+	if (topScroll >=  $(window).height() - 2*$('#ribbon').height()){
+		$('#ribbon').css({
+			'transform' : 'translate(0px, '+ 0 +'px)'
+		});
+	}
+})
